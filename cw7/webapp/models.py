aa2876ca3037cb8f9d93c5e18c535b219eaec571
aza <azamuzapbar@gmail.com>
2023-02-25 01:30:13 -0500

    Создал модель + регистрация на админ панели + добавление фикстур

--- cw7/webapp/models.py
+++ cw7/webapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Article(models.Model):
+    CHOICES = (
+        ('active', 'Активно'),
+        ('blocked', 'Заблокировано'),
+    )
+
+    title = models.CharField(verbose_name='название', max_length=200, null=False, blank=False)
+    mail = models.EmailField(verbose_name='почта', max_length=200, null=False,blank=False)
+    text = models.TextField(verbose_name='описание', max_length=3000, null=False, blank=False)
+    datetime = models.DateTimeField(auto_now_add=True, verbose_name='дата создание')
+    updated = models.DateTimeField(auto_now=True,verbose_name='Дата изменения')
+    status = models.CharField(max_length=10, choices=CHOICES, default='active')
+
+    def __str__(self):
+        return f'{self.title} - {self.text} - {self.mail} - {self.status}'
